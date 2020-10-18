465bf14e0c9636b6ee02434db5010c92b9a45a5e
casa98 <cagudeloa@uninorte.edu.co>
2020-10-18 12:58:10 -0500

    Setup Retrofit

--- app/src/main/java/com/cagudeloa/newsapp/api/NewsAPI.kt
+++ app/src/main/java/com/cagudeloa/newsapp/api/NewsAPI.kt
@@ -1,4 +1,44 @@
 package com.cagudeloa.newsapp.api
 
-interface NewsApi {
+import com.cagudeloa.newsapp.NewsResponse
+import com.cagudeloa.newsapp.api.ApiKey.Companion.API_kEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    /**
+     * STEPS TO MAKE NETWORK REQUESTS USING RETROFIT:
+     * - Create the response objects we need (done in this file), we'll get the answer as JSON strings.
+     * - Create kotlin objects that represent those answers
+     *   (data classes using the cool extension, e.g. I call it, NewsResponse, check it out).
+     *
+     * This file will be used to define our single requests we'll execute from code
+     */
+
+    // To bring all breaking news from API, it's a GET request
+    @GET("v2/top-headlines")    // Specify the part after BASE_URL
+    suspend fun getBreakingNews(
+        // Let's add some parameters
+        @Query("country")       // These details vary from API to API, check their docs
+        countryCode: String = "co",
+        // Each page contains 20 items, how many will it bring at once? Once 1 (20 news)
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")    // Because this app required API key
+        apiKey: String = API_kEY
+    ): Response<NewsResponse>   // What this will return
+
+
+    // To make our Search requests
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q") // I'll get this parameter directly from the view
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_kEY
+    ): Response<NewsResponse>
+
 }
---
+++ app/src/main/java/com/cagudeloa/newsapp/api/RetrofitInstance.kt
@@ -0,0 +1,37 @@
+package com.cagudeloa.newsapp.api
+
+import com.cagudeloa.newsapp.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object {
+
+        /**
+         * My Retrofit singleton class, to make calls from everywhere of the code
+         */
+
+        private val retrofit by lazy {
+            // For logging responses, useful for debugging
+            val logging = HttpLoggingInterceptor()  // Attach it to the retrofit object later
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY) // Body, the whole response itself
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            // Now use the client and pass it to the retrofit instance
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()) // To convert response to objects Kotlin understands
+                .client(client)     // Attached
+                .build()
+
+        }
+
+        // This is what I'll use in our code to make our network requests and bring the data
+        val api: NewsAPI by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
+}
