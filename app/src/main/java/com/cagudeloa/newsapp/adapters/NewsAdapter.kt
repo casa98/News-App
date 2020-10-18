8b00c158adb05a2536bb8a8176d3db2565764f71
casa98 <cagudeloa@uninorte.edu.co>
2020-10-18 16:20:21 -0500

    Setup NewsAdapter with clickListener using diffUtil

---
+++ app/src/main/java/com/cagudeloa/newsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,78 @@
+package com.cagudeloa.newsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.cagudeloa.newsapp.R
+import com.cagudeloa.newsapp.models.Article
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    /**
+     * For the RecyclerView, we'll be using DiffUtil, like an efficient way to update one element
+     * of the recycler, instead of the entire list of items being sent in the constructor.
+     */
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            // If the items have the same URL, then they're the same and nothing changed, returns true
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            // This functions compare the content of an oldItem and a newItem
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    /**
+     * Done with diffUtil
+     */
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview, parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Picasso.get().load(article.url).fit().centerCrop()
+                .error(R.drawable.ic_launcher_background).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
+    }
+
+    // Lambda function. Unit when nothing is returned
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
