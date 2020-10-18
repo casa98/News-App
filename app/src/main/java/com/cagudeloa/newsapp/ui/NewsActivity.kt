ebf5808ed55e3e55c0591c43ed371ce0d51b0b1a
casa98 <cagudeloa@uninorte.edu.co>
2020-10-18 11:38:28 -0500

    Setup bottom navigation (easily)

--- app/src/main/java/com/cagudeloa/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/cagudeloa/newsapp/ui/NewsActivity.kt
@@ -2,11 +2,17 @@ package com.cagudeloa.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.cagudeloa.newsapp.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        // Setup bottom navigation with the navigation component
+        bottomNavigationView.setupWithNavController(nav_host_fragment.findNavController())
     }
 }
